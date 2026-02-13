662831eebac3fc24a18b4e9441e2e0b8334358e5
Leona Zheng <77574933+ChupapiMunanyo@users.noreply.github.com>
2026-02-14 00:41:29 +0800

    changes

--- script.js
+++ script.js
@@ -54,15 +54,15 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
+    "Are you sure?🥺",
+    "Really sure??🥺🥺",
+    "Are you positive?😣",
+    "Beta please...",
     "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
+    "If you say no, I will be really sad...💔",
+    "I will be very sad...💔",
+    "I will be very very very sad...😭😭😭",
+    "Okay fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
 ];
 
